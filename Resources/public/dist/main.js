a3a55b269cb0b311a9c007d3939666cd32350267
Thomas Schedler <thomas@chirimoya.at>
2016-08-26 14:50:54 +0200

    added build

---
+++ Resources/public/dist/main.js
@@ -0,0 +1,1 @@
+require.config({paths:{sulucomment:"../../sulucomment/dist",sulucommentcss:"../../sulucomment/css"}}),define(function(){"use strict";return{name:"Sulu Comment Bundle",initialize:function(a){a.components.addSource("sulucomment","/bundles/sulucomment/dist/components")}}});

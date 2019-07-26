75fdd0c6d0f0c2c32aec384d74066f6aa1d2de36
Niklas Natter <niklas@sulu.io>
2019-07-26 10:38:12 +0200

    remove unnecessary javscript files

--- Resources/public/dist/services/threads/router.js
+++
@@ -1,1 +0,0 @@
-define(["services/husky/util","services/husky/mediator"],function(a,b){"use strict";function c(){}var d=null,e=function(){return null===d&&(d=new c),d},f=function(a){b.emit("sulu.router.navigate",a,!0,!0)};return c.prototype={toList:function(){f("threads")},toEdit:function(a,b){f("threads/edit:"+a+"/"+(b||"details"))}},e()});

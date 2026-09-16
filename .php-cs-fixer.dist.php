940c3875c5deb2f05fcee826422299be8989b848
Pascal CESCON - Amoifr <pascal.cescon@gmail.com>
2026-09-16 11:02:51 +0200

    Ignore the config reference generated from Symfony 8.1

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,6 +11,7 @@ EOF;
 
 $finder = PhpCsFixer\Finder::create()
     ->exclude(['var/cache', 'tests/Resources/cache', 'node_modules'])
+    ->notPath('Tests/Application/config/reference.php')
     ->in(__DIR__);
 
 $config = new PhpCsFixer\Config();
